93bf6c22fa80d5186e54e0131a97f0d0583d394c
Shivansh2203 <168472217+Shivansh2203@users.noreply.github.com>
2025-08-17 20:45:41 +0530

    add url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/url/shorten", {
+      const response = await fetch("https://url-shortener-9qnl.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ longUrl }),
